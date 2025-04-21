f4cdd0179b929e21f2912785454bd83b6d4966f6
Vivek Sharma <viveksharma090297@gmail.com>
2025-04-21 12:33:04 +0530

    debug router issues

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import ShopContextProvider from "./context/ShopContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Ecomerce-react">
     <ShopContextProvider>
       <App />
     </ShopContextProvider>
